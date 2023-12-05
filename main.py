0458b5469822c1c5c6fbe19df4749ce13896c6c1
laurensart <artoflaurens@gmail.com>
2023-12-05 10:57:16 +0100

    added teleport and collision

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ class Alien:
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
         self.health = 3
-
     def hit(self):
         self.health -= 1
 
     def is_alive(self):
         return self.health > 0
+
+    def teleport(self, new_x, new_y):
+        self.x_coordinate = new_x
+        self.y_coordinate = new_y
+
+    def collision_detection(selfself, other_object):
+        pass
