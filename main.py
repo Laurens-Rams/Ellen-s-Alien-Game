71c63fc0f82e08c5177de483d1ec1d0c763d7b4e
laurensart <artoflaurens@gmail.com>
2023-12-05 10:58:30 +0100

    create aliens

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 class Alien:
+    total_aliens_created = 0
     def __init__(self, x_coordinate, y_coordinate):
         self.x_coordinate = x_coordinate
         self.y_coordinate = y_coordinate
         self.health = 3
+        Alien.total_aliens_created += 1
     def hit(self):
         self.health -= 1
 
@@ -15,3 +17,8 @@ class Alien:
 
     def collision_detection(selfself, other_object):
         pass
+
+def new_aliens_collection(start_positions):
+    return [Alien(x, y) for x, y in start_positions]
+
+
